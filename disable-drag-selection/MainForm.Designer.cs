9ee74066eb37d355aa3f5d12d5dbcbe95ec78549
Tom Gregor <tgregor@ivsoftware.com>
2024-09-26 03:27:27 -0600

    Add project files.

---
+++ disable-drag-selection/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace disable_drag_selection
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView = new DataGridViewEx();
+            ((System.ComponentModel.ISupportInitialize)dataGridView).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView.Dock = DockStyle.Fill;
+            dataGridView.Location = new Point(10, 10);
+            dataGridView.Name = "dataGridView";
+            dataGridView.RowHeadersWidth = 62;
+            dataGridView.Size = new Size(458, 224);
+            dataGridView.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(478, 244);
+            Controls.Add(dataGridView);
+            Name = "MainForm";
+            Padding = new Padding(10);
+            Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)dataGridView).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridViewEx dataGridView;
+    }
+}
---
+++ disable-drag-selection/MainForm.cs
@@ -0,0 +1,57 @@
+
+using IVSoftware.Portable;
+using System.ComponentModel;
+
+namespace disable_drag_selection
+{
+    public partial class MainForm : Form
+    {
+        public MainForm() => InitializeComponent();
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            
+            dataGridView.DataSource = Records;
+            dataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            for (int i = 0; i< 10; i++)
+            {
+                Records.AddNew();
+            }
+        }
+        BindingList<Record> Records { get; } = new BindingList<Record>();
+    }
+    
+    public class Record
+    {
+        static int _debugCount = 1;
+        public Record()
+        {
+            col1 = col2 = col3 = _debugCount ++;
+        }
+
+        public int col1 { get; set; }
+        public int col2 { get; set; }
+        public int col3 { get; set; }
+    }
+    class DataGridViewEx : DataGridView
+    {
+        protected override void SetSelectedCellCore(int columnIndex, int rowIndex, bool selected)
+        {
+            bool disabledByOneCellPerColumnRule =
+                ModifierKeys == Keys.Control &&
+                SelectedCells.OfType<DataGridViewCell>().Any(_ => _.ColumnIndex == columnIndex);
+
+            base.SetSelectedCellCore(
+                columnIndex, 
+                rowIndex,
+                selected && !(disabledByOneCellPerColumnRule || _wdtMove.Running));
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (MouseButtons == MouseButtons.Left) _wdtMove.StartOrRestart();
+            base.OnMouseMove(e);
+        }
+        // <PackageReference Include="IVSoftware.Portable.WatchdogTimer" Version="1.2.1" />
+        WatchdogTimer _wdtMove = new WatchdogTimer{ Interval = TimeSpan.FromMilliseconds(250)};
+    }
+}
